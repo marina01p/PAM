a754aa6b4f19abe4fdad7d8f49b084db428afe54
marina01p <56072634+marina01p@users.noreply.github.com>
2021-02-05 02:38:15 +0200

    add 2nd activity screen

--- Laboratory1/app/src/main/java/com/example/laboratory1/MainActivity.kt
+++ Laboratory1/app/src/main/java/com/example/laboratory1/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.laboratory1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val enterDestinationTxt = findViewById<EditText>(R.id.enterDestinationTxt)
+        val navigateBtn= findViewById<Button>(R.id.navigateBtn)
+
+        navigateBtn.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("destinationName", enterDestinationTxt.text.toString());
+            startActivity(intent)
+        }
     }
 }
---
+++ Laboratory1/app/src/main/java/com/example/laboratory1/SecondActivity.kt
@@ -0,0 +1,19 @@
+package com.example.laboratory1
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val intent = intent
+        val receivedEmail = intent.getStringExtra("destinationName")
+
+        val textViewMessage = findViewById<TextView>(R.id.textViewMessage)
+
+        textViewMessage.text="Welcome to "+ receivedEmail
+    }
+}
