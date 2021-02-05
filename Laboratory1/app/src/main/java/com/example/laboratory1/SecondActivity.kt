52c52e413e846649638fa65008bb356b13365d61
marina01p <56072634+marina01p@users.noreply.github.com>
2021-02-05 03:29:51 +0200

    add go back btn

--- Laboratory1/app/src/main/java/com/example/laboratory1/SecondActivity.kt
+++ Laboratory1/app/src/main/java/com/example/laboratory1/SecondActivity.kt
@@ -1,19 +1,26 @@
 package com.example.laboratory1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        val intent = intent
+        /////////////////////////////////////////
         val receivedDestination = intent.getStringExtra("destinationName")
 
         val textViewMessage = findViewById<TextView>(R.id.textViewMessage)
+        val goBackBtn = findViewById<Button>(R.id.goBackBtn)
 
         textViewMessage.text="Welcome to "+ receivedDestination
+
+        goBackBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
