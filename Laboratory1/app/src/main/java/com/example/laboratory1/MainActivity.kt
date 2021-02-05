1ade99a27842067dc2424c64418140821ddb6d6b
marina01p <56072634+marina01p@users.noreply.github.com>
2021-02-05 03:38:54 +0200

    change interaction between activities

--- Laboratory1/app/src/main/java/com/example/laboratory1/MainActivity.kt
+++ Laboratory1/app/src/main/java/com/example/laboratory1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -15,6 +16,11 @@ class MainActivity : AppCompatActivity() {
 
         val enterDestinationTxt = findViewById<EditText>(R.id.enterDestinationTxt)
         val navigateBtn= findViewById<Button>(R.id.navigateBtn)
+        val textView2 = findViewById<TextView>(R.id.textView2)
+
+        val receivedCity = intent.getStringExtra("cityName")
+
+        textView2.text="Last visit: "+ receivedCity
 
         navigateBtn.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
--- Laboratory1/app/src/main/java/com/example/laboratory1/SecondActivity.kt
+++ Laboratory1/app/src/main/java/com/example/laboratory1/SecondActivity.kt
@@ -3,6 +3,7 @@ package com.example.laboratory1
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,11 +16,13 @@ class SecondActivity : AppCompatActivity() {
 
         val textViewMessage = findViewById<TextView>(R.id.textViewMessage)
         val goBackBtn = findViewById<Button>(R.id.goBackBtn)
+        val txtArea = findViewById<EditText>(R.id.txtArea)
 
         textViewMessage.text="Welcome to "+ receivedDestination
 
         goBackBtn.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("cityName", txtArea.text.toString());
             startActivity(intent)
         }
     }
