694af6f948416ceb363e4900f7cc189977ec5afb
marina01p <56072634+marina01p@users.noreply.github.com>
2021-02-27 19:51:22 +0200

    add decorative authentication

--- Laboratory1/app/src/main/java/com/example/laboratory1/MainActivity.kt
+++ Laboratory1/app/src/main/java/com/example/laboratory1/MainActivity.kt
@@ -10,25 +10,30 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val registerGoTxt = findViewById<TextView>(R.id.registerGoTxt)
         val navigateBtn = findViewById<Button>(R.id.navigateBtn)
-        val goToLists = findViewById<Button>(R.id.goToLists)
+        val usernameLogArea = findViewById<EditText>(R.id.usernameLogArea)
+        val passwordLogArea = findViewById<EditText>(R.id.passwordLogArea)
 
         navigateBtn.setOnClickListener {
+            if(usernameLogArea.text.trim().isNotEmpty() && passwordLogArea.text.trim().isNotEmpty()) {
+                val intent = Intent(this, ThirdActivity::class.java)
+                startActivity(intent)
+//                Toast.makeText(this,"Input provided",Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this,"Input required",Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+        registerGoTxt.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
-
-        goToLists.setOnClickListener {
-            val intent = Intent(this, ThirdActivity::class.java)
-            startActivity(intent)
-        }
-
-
     }
 }
--- Laboratory1/app/src/main/java/com/example/laboratory1/SecondActivity.kt
+++ Laboratory1/app/src/main/java/com/example/laboratory1/SecondActivity.kt
@@ -4,10 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
@@ -16,12 +13,31 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
+        val textView = findViewById<TextView>(R.id.textView)
+        val signGoTxt = findViewById<TextView>(R.id.signGoTxt)
         val goBackBtn = findViewById<Button>(R.id.goBackBtn)
-
-
+        val usernameRegArea = findViewById<EditText>(R.id.usernameRegArea)
+        val emailRegArea = findViewById<EditText>(R.id.emailRegArea)
+        val passwordRegArea = findViewById<EditText>(R.id.passwordRegArea)
+        val repeatPasswordRegArea = findViewById<EditText>(R.id.repeatPasswordRegArea)
 
         goBackBtn.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+            if (usernameRegArea.text.trim().isNotEmpty() && emailRegArea.text.trim().isNotEmpty()
+                    && passwordRegArea.text.trim().isNotEmpty() && repeatPasswordRegArea.text.trim().isNotEmpty()) {
+
+                if (passwordRegArea.text.trim() == repeatPasswordRegArea.text.trim()) {
+                    val intent = Intent(this, ThirdActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "Passwords do not match", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(this, "Input required", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        signGoTxt.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
     }
