38bf0dc32a82db873fab76d2a0e7e47e268ea7d9
marina01p <56072634+marina01p@users.noreply.github.com>
2021-04-22 05:01:27 +0300

    edit

--- Laboratory1/app/src/main/java/com/example/laboratory1/SecondActivity.kt
+++ Laboratory1/app/src/main/java/com/example/laboratory1/SecondActivity.kt
@@ -3,14 +3,8 @@ package com.example.laboratory1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
 import com.example.laboratory1.databinding.ActivitySecondBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.activity_second.*
-import java.util.*
-import kotlin.collections.ArrayList
 
 open class SecondActivity : AppCompatActivity() {
 
